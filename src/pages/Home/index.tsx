9e442326e87047a28b4eb78d9b24a281b89f3cd3
Jackson <jackson1712@hotmail.com>
2023-07-19 17:18:41 -0300

    refactor: change items to receive new features and styles at pages

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,29 @@
 import { Header } from '../../components/Header'
-import { HomeContainer } from './styles'
+import { Profile } from '../../components/Profile'
+import { Post } from './components/Post'
+import { Content, HomeContainer, Search } from './styles'
 
 export function Home() {
   return (
     <HomeContainer>
       <Header />
+      <Content>
+        <Profile />
+
+        <Search>
+          <div>
+            <strong>Publicações</strong>
+            <span>6 publicações</span>
+          </div>
+
+          <input type="text" placeholder="Buscar conteúdo" />
+        </Search>
+
+        <div>
+          <Post />
+          <Post />
+        </div>
+      </Content>
     </HomeContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,47 @@
 import { styled } from 'styled-components'
 
-export const HomeContainer = styled.div``
+export const HomeContainer = styled.div`
+  width: 100%;
+`
+
+export const Content = styled.main`
+  max-width: 58rem;
+  padding: 0 2rem;
+  margin: 0 auto;
+`
+
+export const Search = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  margin-top: 4.5rem;
+  gap: 0.75rem;
+
+  > div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    strong {
+      font-family: 'Nunito', sans-serif;
+      font-size: 1.125rem;
+      font-weight: bold;
+      line-height: 1.6;
+      color: ${({ theme }) => theme['base-subtitle']};
+    }
+
+    span {
+      font-size: 0.875rem;
+      line-height: 1.6;
+      color: ${({ theme }) => theme['base-span']};
+    }
+  }
+
+  > input {
+    border: 1px solid ${({ theme }) => theme['base-border']};
+    padding: 0.75rem 1rem;
+    border-radius: 0.375rem;
+    background: ${({ theme }) => theme['base-input']};
+    color: ${({ theme }) => theme['base-title']};
+  }
+`
